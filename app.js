daf571faa83ebf7518ea46dfef873e19bea6f228
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-05-12 21:57:47 -0700

    signIn, signUp routes added

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var signUpRouter = require('./routes/signUp');
+var signInRouter = require('./routes/signIn');
 
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/signUp', signUpRouter);
+app.use('/signIn', signInRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
