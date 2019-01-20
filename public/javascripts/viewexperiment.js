cecf46a668bfe53615e7aefc4aee7efaee80e269
Umang Mathur <umangmathur10051992@gmail.com>
2019-01-20 12:21:14 -0800

    ui logic fixes. Removed code for cropping

--- public/javascripts/viewexperiment.js
+++ public/javascripts/viewexperiment.js
@@ -1,187 +1,125 @@
+//IDs of all selected(ticked) checkboxes above each image(Check Box Image Top).
+//Example: cbIt1, cbIt2, cbIt3
+let selectedImageTopCheckBoxIds = new Set(); 
+//IDs of all images for whom the checkbox above image is selected(ticked)
+//Example: 1, 2, 3, 4 (The values of this set correspond to the set of 'selectedImageTopCheckBoxIds')
+let selectedImageIds = new Set(); 
+
+//IDs of all selected toggle buttons below each image(toggle button is actually a checkbox with CSS)
+//Example: cbToggle1, cbToggle2, cbToggle3
+let selectedImagesCropToggleCheckBoxIds = new Set(); 
+//IDs of all images for whom the crop toggle button is Selected(True). i.e, the images which have been selected for cropping
+//Example: 1, 2, 3, 4 (The values of this set correspond to the set of 'selectedImagesCropToggleCheckBoxIds')
+let selectedImagesForCroppingIds = new Set(); 
+
+//TODO: 4 sets are being maintained above. This number can be reduced to 2.
+
 $( document ).ready(function() {
-    console.log( "ready!" );
-    $('.cropBtn').attr('disabled', true);
-    let rect;
-    const cropInstance = [];
-    let lock = false;
-    const callback = function (id, value) {
-        console.log("cropbox moved in from image# " + id);
-        if (!lock) {
-            lock = true;
-            cropInstance.forEach(function (cropper, index) {
-                if (index !== id) {
-                    console.log("moving cropbox " + index);
-                    // cropper.moveTo(value.x, value.y);
-                    cropper.resizeTo(value.width, value.height);
-                    cropper.moveTo(value.x, value.y);
-                }
-                if (cropInstance.length - 1 === index) lock = false;
-            })
-        }
-    };
-    let images = $(".large-image");
-    for (let i = 0; i < images.length; i++) {
-        let cropper = new Croppr(images[i], {
-            startSize: [100, 100, 'px'],
-            returnMode: "raw",
-            onCropEnd: function (value) {
-                console.log(value.x, value.y, value.width, value.height);
-                let top = value.y;
-                let right = value.x + value.width;
-                let bottom = value.y + value.height;
-                let left = value.x;
-                let $clip = $('#clip');
-                $clip.css('position', 'absolute');
-                $clip.css('top', -1 * top);
-                $clip.css('left', -1 * left);
-                rect = 'rect(' + top + 'px, ' + right + 'px, ' + bottom + 'px, ' + left + 'px)';
-                console.log(rect);
-                $clip.css('clip', rect);
-                callback(i, value)
-            }
-        });
-        cropInstance.push(cropper);
-    }
-    
-    // crop checkboxes -- gets's id of the enabled checkboxes
-    let selected;
-    $checkbox = $('.chk');
-    $checkbox.click(checkArray(this, selected));
-    // enable all crop checkboxes
-    let clicked = false;
-    $(".checkall").on("click", function () {
-        ({ clicked, selected } = enableAllCropCheckBoxes(clicked, selected));
+    // Disable all crop buttons on page load
+    $('.cropBtn').prop('disabled', true);
+    // Click listener for class 'checkboxImageTop'
+    $('.checkboxImageTop').click(function () {
+        let clickedCheckBox = this;
+        let checkBoxId = clickedCheckBox.id;
+        updateSelectedItems(checkBoxId);
     });
-            
-    // crop button
-    $("#cropBtn").click(function (e) {
-        onCropButtonCLick(e, rect, selected);
+    // Click listener for 'Crop' button beneath each image
+    $('.checkBoxToggleCrop').click(function () {
+        let clickedToggleCheckBox = this;
+        let toggleCheckBoxId = clickedToggleCheckBox.id;
+        let cropButtonId = toggleCheckBoxId.replace('cbToggle', 'btnCrop');
+        toggleCropButtonEnablement(toggleCheckBoxId, cropButtonId);
     });
-    // select images
-    $checkbox = $('.checkhour');
-    let ckArray = [];
-    $checkbox.click(function() {
-        checkArray1(ckArray);
+    // Click listener for 'Select All' button
+    $('#btnSelectAll').click(function () {
+        checkAllImageTopCheckBoxes(true);
     });
-    
-    // select all images using checkbox
-    let click = false;
-    $(".uncheck").on("click", function () {
-        ({ click, ckArray } = selectAllImages(click, ckArray));
+    // Click listener for 'Unselect All' button
+    $('#btnUnselectAll').click(function () {
+        checkAllImageTopCheckBoxes(false);
     });
-    
-    // gets id of the images to delete on delete button
+    $('#btnEnableCrop').click(function () {
+        enableAllCropButtons(true);
+    });
+    $('#btnDisableCrop').click(function () {
+        enableAllCropButtons(false);
+    });
     $("#deleteBtn").click(function (e) {
-        deleteImages(e, ckArray);
+        deleteImages(e);
+    });
+    $('.cropBtn').click(function() {
+        let imageIdArray = Array.from(selectedImagesForCroppingIds);
+        window.alert('Fire event here !!' + '\nIDs of images to be cropped are: ' + imageIdArray);
     });
 });
 
-function checkArray(checkBox, selected) {
-    let chkArray = [];
-    $(".chk:checked").each(function () {
-        chkArray.push($(checkBox).val());
-    });
-    selected = chkArray.join(',');
-    /* check if there is selected checkboxes, by default the length is 1 as it contains one single comma */
-    if (selected.length > 0) {
-        document.getElementById(checkBox.id.replace('cb', 'bt')).disabled = false;
-        console.log(chkArray);
-    }
-    else {
-        document.getElementById(checkBox.id.replace('cb', 'bt')).disabled = true;
-        console.log(chkArray);
-    }
-    return selected;
+function enableAllCropButtons(isCropEnabled) {
+    $('.checkBoxToggleCrop').prop('checked', isCropEnabled);
+    $('.cropBtn').attr('disabled', !isCropEnabled);
+    if(isCropEnabled) {
+        let allcheckBoxToggleCropList = document.querySelectorAll('.checkBoxToggleCrop');
+        for (i = 0; i < allcheckBoxToggleCropList.length; i++) {
+            let toggleCheckBoxId  = allcheckBoxToggleCropList[i].id;
+            let imageId = toggleCheckBoxId.replace('cbToggle', '');
+            selectedImagesCropToggleCheckBoxIds.add(toggleCheckBoxId);
+            selectedImagesForCroppingIds.add(imageId);
+        }
+    } else {
+        selectedImagesCropToggleCheckBoxIds.clear();
+        selectedImagesForCroppingIds.clear();
+    } 
 }
 
-function enableAllCropCheckBoxes(clicked, selected) {
-    $(".chk").prop("checked", !clicked);
-    clicked = !clicked;
-    let chkArray = [];
-    $(".chk:checked").each(function () {
-        chkArray.push($(this).val());
-    });
-    selected = chkArray.join(',');
-    /* check if there is selected checkboxes, by default the length is 1 as it contains one single comma */
-    if (selected.length > 0) {
-        console.log("enable all crop buttons: " + chkArray);
-        $('.cropBtn').attr('disabled', false);
+function checkAllImageTopCheckBoxes(isChecked) {
+    let allImageTopCheckBoxesList = document.querySelectorAll('.checkboxImageTop');
+    for (i = 0; i < allImageTopCheckBoxesList.length; i++) {
+        let checkBoxId  = allImageTopCheckBoxesList[i].id;
+        $('#'+checkBoxId).prop('checked', isChecked);
+        updateSelectedItems(checkBoxId);
     }
-    else {
-        console.log("disable all crop buttons: " + chkArray);
-        $('.cropBtn').attr('disabled', true);
-    }
-    return { clicked, selected };
 }
 
-function onCropButtonCLick(e, rect, selected) {
+function toggleCropButtonEnablement(toggleCheckBoxId, cropButtonId) {
+    let imageId = toggleCheckBoxId.replace('cbToggle', '');
+    let isChecked = $('#'+toggleCheckBoxId).is(':checked'); 
+    $('#'+cropButtonId).prop("disabled", isChecked ? false : true);
+    if(isChecked) {
+        selectedImagesCropToggleCheckBoxIds.add(toggleCheckBoxId);
+        selectedImagesForCroppingIds.add(imageId);
+    } else {
+        selectedImagesCropToggleCheckBoxIds.delete(toggleCheckBoxId);
+        selectedImagesForCroppingIds.delete(imageId);
+    }
+}
+
+function updateSelectedItems(clickedCheckBoxId) {
+    let imageId = clickedCheckBoxId.replace('cbIt', '');
+    let isChecked = $('#'+clickedCheckBoxId).is(':checked'); 
+    console.log(isChecked);
+    if(isChecked) {
+        selectedImageIds.add(imageId);
+        selectedImageTopCheckBoxIds.add(clickedCheckBoxId);
+    } else {
+        selectedImageIds.delete(imageId);
+        selectedImageTopCheckBoxIds.delete(clickedCheckBoxId);
+    }
+    console.log(selectedImageIds);
+    let imageIdArray = Array.from(selectedImageIds);
+    $("#demo").text(imageIdArray);
+    $("#demo1").text("Are you sure you want to delete " + imageIdArray.length + " image(s) ?");
+    if (imageIdArray.length === 0) {
+        $("#demo1").text("No Image Selected");
+    }
+}
+
+function deleteImages(e) {
     e.preventDefault();
-    const data = { rect: rect };
-    const data1 = { selected: selected };
-    console.log(data);
-    console.log(data1);
-    $.ajax({
-        type: "POST",
-        url: window.location.href + "/cropImages",
-        data: { data1: JSON.stringify(data1), data: JSON.stringify(data) },
-        success: function (result) {
-            location.href = window.location.href;
-        },
-        error: function (err) {
-            console.log(err);
-        }
-    });
-}
-
-function checkArray1(ckArray) {
-    ckArray = $.map($checkbox, function (el) {
-        if (el.checked) {
-            return el.id;
-        }
-    });
-    console.log(ckArray);
-    document.getElementById("demo").innerHTML = ckArray;
-    if (ckArray.length === 0) {
-        document.getElementById("demo1").innerHTML = "No Image Selected";
-    }
-    if (ckArray.length === 1) {
-        document.getElementById("demo1").innerHTML = "Are you sure you want to delete " + ckArray.length + " image?";
-    }
-    else {
-        document.getElementById("demo1").innerHTML = "Are you sure you want to delete " + ckArray.length + " images?";
-    }
-    return ckArray;
-}
-
-function selectAllImages(click, ckArray) {
-    $(".checkhour").prop("checked", !click);
-    click = !click;
-    ckArray = $.map($checkbox, function (el) {
-        if (el.checked) {
-            return el.id;
-        }
-    });
-    console.log(ckArray);
-    document.getElementById("demo").innerHTML = ckArray;
-    if (ckArray.length === 1) {
-        document.getElementById("demo1").innerHTML = "Are you sure you want to delete " + ckArray.length + " image?";
-    }
-    else {
-        document.getElementById("demo1").innerHTML = "Are you sure you want to delete " + ckArray.length + " images?";
-    }
-    if (ckArray.length === 0) {
-        document.getElementById("demo1").innerHTML = "No Image Selected";
-    }
-    return { click, ckArray };
-}
-
-function deleteImages(e, ckArray) {
-    e.preventDefault();
-    const data = { foo: ckArray };
+    let imageIdArray = Array.from(selectedImageIds);
+    const data = { foo: imageIdArray };
     $.ajax({
         type: "POST",
         url: window.location.href + "/deleteImages",
-        data: { images: JSON.stringify(ckArray) },
+        data: { images: JSON.stringify(imageIdArray) },
         success: function (result) {
             location.href = window.location.href;
             //                location.reload()
@@ -190,13 +128,4 @@ function deleteImages(e, ckArray) {
             console.log(err);
         }
     });
-}
-
-function each(arr, callback) {
-    let length = arr.length;
-    let i;
-    for (i = 0; i < length; i++) {
-        callback.call(arr, arr[i], i, arr);
-    }
-    return arr;
 }
